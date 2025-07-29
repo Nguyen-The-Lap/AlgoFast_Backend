15c2400829a607525bd244ff52f4a80b67161cc4
Nguyen-The-Lap <91041708+Nguyen-The-Lap@users.noreply.github.com>
2025-07-30 00:12:45 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,9 +16,14 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://algofast-frontend.onrender.com'],
+  origin: [
+    'http://localhost:3000',
+    'https://algofast-frontend.onrender.com',
+    'https://algo-fast-frontend.vercel.app'  
+  ],
   credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(session({

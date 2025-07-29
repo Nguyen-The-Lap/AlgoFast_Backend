0e4f912e4b796f55e71d8d397abaf494e9ac3624
Nguyen-The-Lap <91041708+Nguyen-The-Lap@users.noreply.github.com>
2025-07-29 18:18:28 +0000

    fixing 401

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://algo-fast-frontend.vercel.app',
+  origin: 'https://your-frontend.vercel.app',
   credentials: true
 }));
 

5336d3c5f2b3e5ea9a782fc7211de886769f1e69
Nguyen-The-Lap <91041708+Nguyen-The-Lap@users.noreply.github.com>
2025-08-02 20:47:58 +0700

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -154,7 +154,7 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select('-password');
     if (!user) return res.status(400).json({ message: 'Email hoặc mật khẩu không đúng' });
 
     const isMatch = await bcrypt.compare(password, user.password);
